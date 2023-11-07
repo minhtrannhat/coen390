972b3107370671956568e53d3ac405f5e7404aa6
Angelo Reoligio <areoligio@gmail.com>
2023-11-07 16:20:54 -0500

    Feat(Android): change launch activity to UserHomescreen and implemented basic layout of activity

--- coen390_android_app/app/src/main/java/com/example/coen390_app/UserHomescreen.java
+++ coen390_android_app/app/src/main/java/com/example/coen390_app/UserHomescreen.java
@@ -1,9 +1,14 @@
 package com.example.coen390_app;
 
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class UserHomescreen extends AppCompatActivity {
 
@@ -11,6 +16,15 @@ public class UserHomescreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_homescreen);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        //actionBar.setDisplayHomeAsUpEnabled(true); //back button
+        //actionBar.setIcon(R.drawable.parkngo_logo); //test icon
+        //actionBar.setDisplayHomeAsUpEnabled(true);
+
 /*
      final Button modeToggleButton = findViewById(R.id.modeToggleButton);
      final Button saveButton = findViewById(R.id.saveButton);
@@ -23,7 +37,43 @@ public class UserHomescreen extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_edit) {
+            // Handle the "Settings" menu item click
+            // You can perform an action here
+            return true;
+        }
+
+        if (id == R.id.action_adminLogin) {
+            Intent intent = new Intent(this, AdminLogin.class);
+            startActivity(intent);
+            return true;
+        }
+
+        if (id == R.id.action_search) {
+            // Handle the search action
+            //performSearch();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
+
+
+
 
 
 

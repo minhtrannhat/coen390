1186a3ad0a892bfed29fbd73cbe60db5a08acc18
minhtrannhat <minh@minhtrannhat.com>
2023-11-14 14:29:25 -0500

    Fix(Android): AdminHomeScreen now has working RecyclerView

--- coen390_android_app/app/src/main/java/com/example/coen390_app/Views/AdminHomeScreen.java
+++ coen390_android_app/app/src/main/java/com/example/coen390_app/Views/AdminHomeScreen.java
@@ -1,35 +1,102 @@
 package com.example.coen390_app.Views;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.coen390_app.Controllers.ParkingLotProfileFirebaseHelper;
+import com.example.coen390_app.Models.ParkingLotProfile;
 import com.example.coen390_app.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class AdminHomeScreen extends AppCompatActivity {
 
+    protected RecyclerView recyclerView;
+    protected ParkingLotProfileFirebaseHelper dbHelper;
     private Button button;
+    private ParkingLotAdapter parkingLotAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home_screen);
 
-        button = findViewById(R.id.to_form_button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openFormPage();
-            }
-        });
+        dbHelper = new ParkingLotProfileFirebaseHelper();
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        buildRecyclerView();
+    }
+
+    private void buildRecyclerView() {
+        recyclerView = findViewById(R.id.parking_lot_profile_list);
+        dbHelper.getParkingLotProfiles(new ParkingLotProfileFirebaseHelper.OnDataLoadedListener() {
+            @Override
+            public void onDataLoaded(List<ParkingLotProfile> parkingLotProfiles) {
+                // Handle the loaded data
+                // This block of code is executed after the data is loaded
+                Map<String, Map<String, Boolean>> occupancyMap = parkingLotProfiles.get(0).occupancy;
+                List<String> occupiedSpots = new ArrayList<>();
+
+                for (Map.Entry<String, Map<String, Boolean>> floorEntry : occupancyMap.entrySet()) {
+                    String floor = floorEntry.getKey();
+                    Map<String, Boolean> spotStatusMap = floorEntry.getValue();
+
+                    // Now, you can iterate through the spotStatusMap for each floor
+                    for (Map.Entry<String, Boolean> spotEntry : spotStatusMap.entrySet()) {
+                        String spot = spotEntry.getKey();
+                        boolean status = spotEntry.getValue();
+
+                        if (status) {
+                            occupiedSpots.add(spot);
+                        }
+
+                        Log.d("UserHomeScreen", "onDataChange: " + "Floor: " + floor + ", Spot: " + spot + ", Status: " + status);
+                    }
+                }
+
+                dbHelper.setCurrentOccupancy(occupiedSpots.size());
+
+                parkingLotAdapter = new ParkingLotAdapter(getApplicationContext(), parkingLotProfiles);
+
+                // Set an OnClickListener on the RecyclerView items
+                parkingLotAdapter.setOnItemClickListener(new ParkingLotAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClick() {
+                        // Handle item click, e.g., launch UserMapInterface activity
+                        openFormPage();
+                    }
+                });
+
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                recyclerView.setAdapter(parkingLotAdapter);
+
+                Log.d("Userhomescreen", "onStart: Current parkingLotList size " + parkingLotProfiles.size());
+            }
+
+            @Override
+            public void onDataError(String errorMessage) {
+                // Handle the error
+                Log.e("UserHomeScreen", "onDataError: " + errorMessage);
+            }
+        });
+    }
+
     public void openFormPage() {
-        Intent intent = new Intent(this, AdminForm.class );
+        Intent intent = new Intent(this, AdminForm.class);
         startActivity(intent);
     }
 }

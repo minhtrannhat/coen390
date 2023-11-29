2b9eb315c6471d84793823bc29aef540a866d28e
minhtrannhat <minh@minhtrannhat.com>
2023-11-28 21:47:35 -0500

    Fix(Android): AdminAddParkingLot Validates Input
    - Removed unused MainActivity.java, now defunct.

--- coen390_android_app/app/src/main/java/com/example/coen390_app/Views/AdminAddParkingLot.java
+++ coen390_android_app/app/src/main/java/com/example/coen390_app/Views/AdminAddParkingLot.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.coen390_app.Controllers.ParkingLotProfileFirebaseHelper;
 import com.example.coen390_app.Models.SecondaryParkingLot;
@@ -20,16 +21,16 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 
 public class AdminAddParkingLot extends AppCompatActivity {
 
     private ParkingLotProfileFirebaseHelper dbHelper;
-    private EditText lotName, address, owner, ownerTel, ownerEmail;;
+    private EditText lotName, address, owner, ownerTel, ownerEmail, postalCode, city, province, country;
     private Button cancelButton, saveButton;
 
     private ArrayList<SecondaryParkingLot> adminlots = new ArrayList<SecondaryParkingLot>();
-
 
 
     @Override
@@ -49,6 +50,10 @@ public class AdminAddParkingLot extends AppCompatActivity {
         owner = findViewById(R.id.owner);
         ownerTel = findViewById(R.id.ownerTel);
         ownerEmail = findViewById(R.id.ownerEmail);
+        postalCode = findViewById(R.id.postalCode);
+        city = findViewById(R.id.city);
+        province = findViewById(R.id.province);
+        country = findViewById(R.id.country);
 
         loadProfileInfo();
 
@@ -63,27 +68,32 @@ public class AdminAddParkingLot extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(AdminAddParkingLot.this, AdminHomeScreen.class);
                 Bundle args = new Bundle();
-                args.putSerializable("ARRAYLIST3",(Serializable)adminlots );
-                intent.putExtra("BUNDLE3",args);
+                args.putSerializable("ARRAYLIST3", (Serializable) adminlots);
+                intent.putExtra("BUNDLE3", args);
                 startActivity(intent);
             }
         });
-        saveButton.setOnClickListener(new View.OnClickListener()  {
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = lotName.getText().toString();
-                String vAddress = address.getText().toString();
-                adminlots.add(new SecondaryParkingLot(name,vAddress,0,"purple"));
-                Intent intent = new Intent(AdminAddParkingLot.this, AdminHomeScreen.class);
-                Bundle args = new Bundle();
-                args.putSerializable("ARRAYLIST3",(Serializable)adminlots );
-                intent.putExtra("BUNDLE3",args);
-                startActivity(intent);
+                if (validatePreferences()) {
+                    String name = lotName.getText().toString();
+                    String vAddress = address.getText().toString();
+                    adminlots.add(new SecondaryParkingLot(name, vAddress, 0, "purple"));
+                    Intent intent = new Intent(AdminAddParkingLot.this, AdminHomeScreen.class);
+                    Bundle args = new Bundle();
+                    args.putSerializable("ARRAYLIST3", (Serializable) adminlots);
+                    intent.putExtra("BUNDLE3", args);
+                    startActivity(intent);
+
+                    Toast.makeText(AdminAddParkingLot.this, "Save Successfully", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(AdminAddParkingLot.this, "Invalid input. Please check.", Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
     }
-
 
 
     private void loadProfileInfo() {
@@ -97,7 +107,7 @@ public class AdminAddParkingLot extends AppCompatActivity {
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()){
+                if (dataSnapshot.exists()) {
 
                     //set textView
                     owner.setText(dataSnapshot.child("lot_owner").getValue(String.class));
@@ -115,5 +125,86 @@ public class AdminAddParkingLot extends AppCompatActivity {
 
     }
 
+    private boolean validatePreferences() {
+        //lotName, address, postalCode, city, country, owner, ownerTel, ownerEmail;
+        String stringLotName = lotName.getText().toString().trim();
+        String stringAddress = address.getText().toString().trim();
+        String stringPostalCode = postalCode.getText().toString().trim();
+        String stringCity = city.getText().toString().trim();
+        String stringProvince = province.getText().toString().trim();
+        String stringCountry = country.getText().toString().trim();
+        String stringOwner = owner.getText().toString().trim();
+        String stringOwnerTel = ownerTel.getText().toString().trim();
+        String stringOwnerEmail = ownerEmail.getText().toString().trim();
 
+        //fields cannot be empty
+        if (stringLotName.isEmpty() ||
+                stringAddress.isEmpty() ||
+                stringPostalCode.isEmpty() ||
+                stringCity.isEmpty() ||
+                stringProvince.isEmpty() ||
+                stringCountry.isEmpty() ||
+                stringOwner.isEmpty() ||
+                stringOwnerTel.isEmpty() ||
+                stringOwnerEmail.isEmpty()) {
+            showToast("Please fill all fields.");
+            return false; //empty fields
+        }
+
+        //other validation criteria
+        //string patterns
+        String alphanumerics = "^[a-zA-Z0-9\\s.-]{1,30}$";
+        String alphabet = "^[a-zA-Z\\s-]{1,35}$";
+        String telNumber = "^[0-9\\s]{1,20}$";
+        String postalCode = "^[a-zA-Z0-9\\s]{1,7}$";
+
+        //data validation
+        if (!stringLotName.matches(alphanumerics)) {
+            showToast("Invalid Parking lot name.");
+            return false;
+        }
+        if (!stringAddress.matches(alphanumerics)) {
+            showToast("Invalid address.");
+            return false;
+        }
+        if (!stringPostalCode.matches(postalCode)) {
+            showToast("Invalid Postal Code.");
+            return false;
+        }
+        if (!stringCity.matches(alphabet)) {
+            showToast("Invalid city name.");
+            return false;
+        }
+        if (!stringProvince.matches(alphabet)) {
+            showToast("Invalid province name.");
+            return false;
+        }
+        if (!stringCountry.matches(alphabet)) {
+            showToast("Invalid country name.");
+            return false;
+        }
+        if (!stringOwner.matches(alphabet)) {
+            showToast("Invalid owner name.");
+            return false;
+        }
+        if (!stringOwnerTel.matches(telNumber)) {
+            showToast("Invalid telephone number.");
+            return false;
+        }
+        if (!Pattern.compile("^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$").matcher(stringOwnerEmail).matches()
+        ) {
+            showToast("Invalid email.");
+            return false;
+        }
+        return true;
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
 }
